5a10e227fd4b0ba58c0bbbbcb7bf327a92f1f7b2
Aas3 <aasaid@ucdavis.edu>
2023-06-06 13:46:20 -0700

    version 3

---
+++ capstone/version3/script.js
@@ -0,0 +1,23 @@
+(function(){
+
+    'use strict';
+
+    document.querySelector('.open').addEventListener('click', function(e){
+        e.preventDefault();
+        document.getElementById('overlay').className = 'showing';
+    });
+
+    // document.getElementById('myAudio').loop = true;
+
+    document.querySelectorAll(".imageHover").addEventListener('hover', function (e){
+        e.preventDefault();
+        document.getElementById('pop').className = 'showing';
+    })
+    
+
+    // let mySound = new Audio('media/forestWithSound (1).mp4')
+    // mySound.play()
+
+    // const audio = new Audio("media/forestWithSound (1).mp4");
+    // audio.play(); 
+})();
