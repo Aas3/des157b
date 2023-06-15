142f26033127e0cb4b5ee6ed3072d369bf28bc7d
Aas3 <aasaid@ucdavis.edu>
2023-06-14 19:14:11 -0700

    large image removed

--- capstone/finalUpdated/script.js
+++ capstone/finalUpdated/script.js
@@ -5,19 +5,58 @@
 
     AOS.init(); 
 
-    
-    document.querySelector('.open').addEventListener('click', function(e){
-        e.preventDefault();
-        document.getElementById('overlay').className = 'showing';
+    document.querySelector('.o-image2').addEventListener('mouseenter', function(e){
+          e.preventDefault();
+          document.getElementsByClassName('caption').className = 'showing';
+      });
+
+    document.querySelector('.o-image2').addEventListener('mouseover', function(e){
+      e.preventDefault();
+      document.getElementsByClassName('caption').className = 'hidden';
     });
+
+
+    // document.querySelector('.open').addEventListener('click', function(e){
+    //     e.preventDefault();
+    //     document.getElementById('overlay').className = 'showing';
+    // });
 
     // document.getElementById('myAudio').loop = true;
 
-    document.querySelector(".popHover").addEventListener('mouseenter', function (e){
-        e.preventDefault();
-        document.getElementById('pop').className = 'showing';
-    })
+    // document.querySelector(".popHover").addEventListener('mouseenter', function (e){
+    //     e.preventDefault();
+    //     document.getElementById('pop').className = 'showing';
+    // })
     
+
+
+  //   document.querySelector('img').addEventListener('mouseover', function(e){
+  //     e.preventDefault();
+  //     document.getElementById('overlay').className = 'showing';
+  // });
+
+
+      containers.forEach(container => {
+        const overlay = container.querySelector('.overlay');
+        
+        // Add event listeners for mouseenter and mouseleave
+        container.addEventListener('mouseenter', function(e){
+          e.preventDefault();
+          overlay.style.opacity = 1; // Show the overlay
+        });
+        
+        container.addEventListener('mouseleave',function(e){
+          e.preventDefault();
+          overlay.style.opacity = 0; // Hide the overlay
+        });
+      });
+  
+  
+  
+  
+  
+  
+  
 
     // css text lib//
     // Wrap every letter in a span
